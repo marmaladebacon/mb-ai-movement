9a1c8b6da0066cc86379f75d295c71f3844b16c7
mko <zaeni.redmage@gmail.com>
2018-10-30 08:04:58 +0000

    initial

---
+++ Assets/_mbLibs/TestScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("This is a test script");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
